1e5b62f13fc03799bcec1e0c04a63c4de2f28679
David Puerta Martos <dpuerta@usal.es>
2022-03-02 21:24:26 +0100

    added functionalities

--- germlineFilter.py
+++ germlineFilter.py
@@ -1,6 +1,7 @@
 # The following two lines will be needed in every python script:
 from intermine.webservice import Service
 from gld1filter import getgld1
+import pandas as pd
 
 service = Service("http://intermine.wormbase.org/tools/wormmine/service")
 
@@ -21,12 +22,23 @@ def germLineGenes():
 
     return result
 
-def createTextFromHash(hash, textname):
+def createTextFromHash(hash, textname, annotation=None):
+
     with open(textname, 'w') as f:
         for key in hash:
             f.write(key+"\n")
             for element in hash[key]:
                 f.write(element)
+            if annotation:
+                for term in annotation[key]:
+                    f.write(term+"\n")
+
+def createTextFromList(array, textname):
+    with open(textname, 'w') as f:
+        for element in array:
+            f.write(element+"\n")
+
+
 
 # create txt with all genes expressed in germline, uncomment to make it work
 # with open('5a_germlineGenes.txt', 'w') as f:
@@ -65,6 +77,23 @@ def kenbox_filter(hash):
                 filteredhash[key] = hash[key]
     return filteredhash
 
+def getAnnotation(gene):
+    template = service.get_template('ind_go_annotation_for_a_given_gene_new')
+
+    # You can edit the constraint values below
+    # A    Gene.symbol
+
+    rows = template.rows(
+        A={"op": "=", "value": gene}
+    )
+    set1 = set()
+    for row in rows:
+        set1.add(row["goAnnotation.qualifier"]+" "+row["goAnnotation.ontologyTerm.name"])
+        print(row["goAnnotation.qualifier"]+" "+row["goAnnotation.ontologyTerm.name"])
+    return list(set1)
+
+
+
 
 germgenes = germLineGenes()
 gld1genes = getgld1()
@@ -75,12 +104,40 @@ germgld1filteredHash = filterHash(filteredHash,gld1genes)
 kengermgld1 = kenbox_filter(germgld1filteredHash)
 kengerm = kenbox_filter(filteredHash)
 
+gld1filtered = filterHash(hash, gld1genes)
+kengld1 = kenbox_filter(gld1filtered)
+
+gld1filtered_1000 = filterHash(hash, gld1genes[0:1000])
+kengld1_1000 = kenbox_filter(gld1filtered_1000)
+
+print(kengld1_1000)
+
 print("Positive GPS-ARM genes->", len(hash))
 print("Positive GPS-ARM genes in germline->", len(filteredHash))
 print("Positive KEN-BOX  genes in germline ->", len(kengerm))
 print("Positive GPS-ARM genes in germline IP gld-1->", len(germgld1filteredHash))
 print("Positive KEN-BOX  genes in germline IP gld-1->", len(kengermgld1))
+print("POSITIVE GPS-ARM genes in gld-1 IP ->", len(gld1filtered))
+print("POSITIVE KEN-BOX genes in gld-1 IP ->", len(kengld1))
+print("POSITIVE GPS-ARM genes in gld-1 IP first 1000 ->", len(gld1filtered_1000))
+print("POSITIVE KEN-BOX genes in gld-1 IP ->", len(kengld1_1000))
+
+print(kengerm)
+kengermannotation = {}
+for gene in kengerm:
+   kengermannotation[gene] = kengerm[gene] + getAnnotation(gene)
+
+df=pd.DataFrame(data=kengermannotation)
+print(df)
+#
+# print(kengermannotation)
+# createTextFromHash(kengerm,"trial.txt",kengermannotation)
 
 
+# #create text list of genes from list, to use in panther
+# l1 = [gene for gene in kengld1_1000]
+#
+# createTextFromList(gld1genes,"gld1.txt")
+# createTextFromList(l1,)
 
-
+#http://pantherdb.org/tools/compareToRefList.jsp
